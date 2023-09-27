2fe485f4b4f9b88e72a4ddfc6672f941a46e99ba
Emon51 <94897729+Emon51@users.noreply.github.com>
2023-09-27 11:00:59 -0700

    Add files via upload

---
+++ Sliding_Window/GFG_Maximum of all subarrays of size k.py
@@ -0,0 +1,14 @@
+#TLE, TC : O(n*k)
+class Solution:
+    #TLE
+    def max_of_subarrays(self,arr,n,k):
+        res = []
+        for i in range(n-k+1):
+            cm = 0
+            for j in range(i, k+i):
+                cm = max(cm, arr[j])
+            res.append(cm)
+        return res
+                
+
+
