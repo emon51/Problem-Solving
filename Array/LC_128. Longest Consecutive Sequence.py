bdba54313d2fe8ad68b9d9d1c02a40c2470715ac
Emon51 <94897729+Emon51@users.noreply.github.com>
2023-10-19 21:10:07 -0700

    Update LC_128. Longest Consecutive Sequence.py

--- Array/LC_128. Longest Consecutive Sequence.py
+++ Array/LC_128. Longest Consecutive Sequence.py
@@ -19,3 +19,25 @@ class Solution:
                     
             res = max(res, cnt)
         return res
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums.sort()
+        res = 0 
+        n = len(nums)
+        
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        
+        cnt = 1
+        for i in range(1, n):
+            if nums[i] -1 == nums[i-1]:
+                cnt += 1
+            elif nums[i] != nums[i-1]:
+                cnt = 1
+            res = max(res, cnt)
+        return res
+
