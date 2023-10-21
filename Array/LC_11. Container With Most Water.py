2bc677b8dbacf72f68b3e6ef3a12d36d5c88d35d
Emon51 <94897729+Emon51@users.noreply.github.com>
2023-10-21 09:40:05 +0600

    Update LC_11. Container With Most Water.py

--- Array/LC_11. Container With Most Water.py
+++ Array/LC_11. Container With Most Water.py
@@ -1,4 +1,17 @@
+#Brute force, TC: O(N^2), SC: O(1)
+class Solution:
+    def maxArea(self, nums: List[int]) -> int:
+        
+        n = len(nums)
+        res = 0
+        for i in range(n):
+            for j in range(i+1, n):
+                res = max(res, min(nums[i], nums[j]) * (j - i))
+        return res
+        
 
+
+#Optimal, TC: O(N), SC: O(1)
 class Solution:
     def maxArea(self, nums: List[int]) -> int:
         
