b3fcd0f8de878eedb38435045e89ba7c9b70ea3c
Emon51 <94897729+Emon51@users.noreply.github.com>
2023-10-31 11:41:35 +0600

    Create LC_7 Reverse Integer.py

---
+++ Bit_Manupulation/LC_7 Reverse Integer.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        
+        sign = -1 if x < 0 else 1
+
+        x = abs(x)
+
+        res = 0
+        while x:
+            res = res*10 + x%10
+            x //= 10
+        
+        res  = sign*res
+        return res if (-2**31 <= res <= 2**31 -1) else 0        
+#========================================================================================#
