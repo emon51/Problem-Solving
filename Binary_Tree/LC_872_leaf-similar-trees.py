6feb8f17d5f072359ea74f75f608e89b1d3511ad
Emon51 <94897729+Emon51@users.noreply.github.com>
2024-01-09 10:05:00 +0600

    Update LC_872_leaf-similar-trees.py

--- Binary_Tree/LC_872_leaf-similar-trees.py
+++ Binary_Tree/LC_872_leaf-similar-trees.py
@@ -1,3 +1,4 @@
+#DFS
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -19,5 +20,36 @@ class Solution:
 
         r1 = dfs(root1)
         r2 = dfs(root2)
-        return r1 == r2
+        return r1 == r2 
+
+#BFS_stack
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        a = self.bfs(root1)
+        b = self.bfs(root2)
+        if a == b:
+            return True
+        else:
+            return False
+    def bfs(self, node):
+        hold = []
+        if not node:
+            return
+        stack = [node]
+        while stack:
+            if not node.left and not node.right:
+                hold.append(node.val)
+            node = stack.pop()
+
+            if node.left is not None:
+                stack.append(node.left)
+            if node.right is not None:
+                stack.append(node.right)
+        return hold
             
