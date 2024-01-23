d72fa716a24ac3a447082736a1d5e503a55a634b
Emon51 <94897729+Emon51@users.noreply.github.com>
2024-01-23 12:38:26 +0600

    Update GeneratingAllSUbsequences.py

--- Recursion/GeneratingAllSUbsequences.py
+++ Recursion/GeneratingAllSUbsequences.py
@@ -1,8 +1,23 @@
 #Generating all subsequences
+
+#Code1
 arr = [1, 2, 3]
-def f(i,  p = []):
+def fun(i, p = []):
+	if i >= len(arr):
+	     print(p)
+	     return
+	#pick	
+	fun(i+1, p + [arr[i]])
+	#not_pick
+	fun(i+1, p)
+	
+fun(0)
+
+#COde2
+arr = [1, 2, 3]
+def fun(i,  p = []):
 	print(p)
 	for j in range(i, len(arr)):
-		f(j + 1, p + [arr[j]])
+		fun(j + 1, p + [arr[j]])
 		
-f(0)
+fun(0)
