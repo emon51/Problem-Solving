8f09ba3ebfc91c3e3402928503b5031c98f124f9
Emon51 <94897729+Emon51@users.noreply.github.com>
2024-01-24 11:59:11 +0600

    Create PrintAllPaths_rootToLeaf.py

---
+++ Binary_Tree/PrintAllPaths_rootToLeaf.py
@@ -0,0 +1,13 @@
+def dfs(root, p = []):
+        if not root.left and not root.right:
+          p.append(root.val)
+          res.append(p)
+          return
+        if root.left:
+          dfs(root.left, p + [root.val]) 
+        if root.right:
+          dfs(root.right, p + [root.val])
+      
+      res = []
+      dfs(root) 
+      print(res)
