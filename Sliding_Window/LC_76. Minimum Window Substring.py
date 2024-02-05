e78dc93b969cab06001889c1f5eb79ec63d22542
Emon51 <94897729+Emon51@users.noreply.github.com>
2024-02-05 11:10:32 +0600

    Update LC_76. Minimum Window Substring.py

--- Sliding_Window/LC_76. Minimum Window Substring.py
+++ Sliding_Window/LC_76. Minimum Window Substring.py
@@ -16,3 +16,32 @@ class Solution:
                         res = s[i:j + 1]
                         prev_len = j - i + 1
         return res
+
+#Accepted
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+
+        tcount = Counter(t)
+        window = {}
+        minlen = float('inf')
+        res = ''
+        reqcount, currcount = len(tcount), 0
+        l = 0
+
+        for r, ch in enumerate(s):
+            window[ch] = window.get(ch, 0) + 1
+            if ch in tcount and window[ch] == tcount[ch]:
+                currcount += 1
+            
+            while currcount == reqcount:
+                if (r - l + 1) < minlen:
+                    minlen = (r - l + 1)
+                    res = s[l: r + 1]
+                
+                window[s[l]] -= 1
+                if s[l] in tcount and window[s[l]] < tcount[s[l]]:
+                    currcount -= 1
+                l += 1
+        return res
+                
