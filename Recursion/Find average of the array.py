b4c7b3a040b9c9811b643258d54cf27628554992
Emon51 <94897729+Emon51@users.noreply.github.com>
2024-03-24 15:31:32 +0600

    Update Find average of the array.py

--- Recursion/Find average of the array.py
+++ Recursion/Find average of the array.py
@@ -9,3 +9,18 @@ def find_average(arr, index, sum):
 my_arr = [1, 2, 3, 4, 5]
 average = find_average(my_arr, 0, 0)
 print(average)
+
+
+#Using tail_recursion find the array sum
+
+def fun(arr, sm = 0, i = 0):
+    if i == len(arr):
+        return sm
+    return fun(arr, sm + arr[i], i + 1)
+    
+    
+    
+
+arr = [1, 2, 3]
+res = fun(arr)
+print(res)
