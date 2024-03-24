b6824cbb91a2e3bae5ea2d1f41864b7358fb41c8
Emon51 <94897729+Emon51@users.noreply.github.com>
2024-03-24 18:43:55 +0600

    Update Print Fibonacci Series In Reverse Order.py

--- Recursion/Print Fibonacci Series In Reverse Order.py
+++ Recursion/Print Fibonacci Series In Reverse Order.py
@@ -9,6 +9,20 @@ n = 10
 fun(n)
 
 #Print Fibonacci Series In Normal Order Using Recursion
-
+def fun(n, a = 1, b = 1):
+    if n == 0:
+        return 
+    print(a, end = ' ')
+    fun(n - 1, b, a + b)
+n = 10
+fun(n)
 
 #Print Fibonacci Series In Reverse Order Using Tecursion
+
+def fun(n, a = 1, b = 1):
+    if n == 0:
+        return 
+    fun(n - 1, b, a + b)
+    print(a, end = ' ')
+n = 10
+fun(n)
