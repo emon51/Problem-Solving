930b414c983c0cc44a14303a405c5bae1e01e753
Emon51 <94897729+emon51@users.noreply.github.com>
2024-04-22 15:55:14 +0600

    Update CodeStudio:  Longest Common Substring.py

--- Dynamic_programming/CodeStudio:  Longest Common Substring.py
+++ Dynamic_programming/CodeStudio:  Longest Common Substring.py
@@ -1,18 +1,20 @@
 
-#TLE
+#Brute Force
+"""
+    Time complexity: O((N * M * min(N, M))
+    Space complexity: O(1)
+    
+    Where 'N' and 'M' are the lengths of the two strings.
+"""
+#Longest common Substring
 def lcs(s1: str, s2: str) -> int:
-    
-    m, n = len(s1), len(s2)
-    def fun(i, j, count):
-        if i == m or j == n:
-            return count
+    ans = 0
+    for i in range(len(s1)):
+        for j in range(len(s2)):
+            k = 0
+            while ((i + k) < len(s1) and (j + k) < len(s2) and s1[i + k] == s2[j + k]):
+                k = k + 1
+            ans = max(ans, k)
+    return ans
 
-        if s1[i] == s2[j]:
-            count = fun(i + 1, j + 1, count + 1)
-    
-        f1 = fun(i + 1, j, 0)
-        f2 = fun(i, j + 1, 0)
 
-        return max(f1, f2, count)
-
-    return fun(0, 0, 0)
