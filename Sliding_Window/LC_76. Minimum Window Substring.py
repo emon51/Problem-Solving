8fd4510a1e9be2baf626af6e792b0e4621a6d470
Emon51 <94897729+emon51@users.noreply.github.com>
2024-05-15 18:25:10 +0600

    Update LC_76. Minimum Window Substring.py

--- Sliding_Window/LC_76. Minimum Window Substring.py
+++ Sliding_Window/LC_76. Minimum Window Substring.py
@@ -1,4 +1,42 @@
-#TLE
+'''
+Given two strings s and t of lengths m and n respectively, return the minimum window 
+substring
+ of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+
+The testcases will be generated such that the answer is unique.
+
+ 
+
+Example 1:
+
+Input: s = "ADOBECODEBANC", t = "ABC"
+Output: "BANC"
+Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+Example 2:
+
+Input: s = "a", t = "a"
+Output: "a"
+Explanation: The entire string s is the minimum window.
+Example 3:
+
+Input: s = "a", t = "aa"
+Output: ""
+Explanation: Both 'a's from t must be included in the window.
+Since the largest window of s only has one 'a', return empty string.
+ 
+
+Constraints:
+
+m == s.length
+n == t.length
+1 <= m, n <= 105
+s and t consist of uppercase and lowercase English letters.
+ 
+
+Follow up: Could you find an algorithm that runs in O(m + n) time?
+'''
+
+#Brute Force 1
 from collections import Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
@@ -16,6 +54,29 @@ class Solution:
                         res = s[i:j + 1]
                         prev_len = j - i + 1
         return res
+
+#Brute Force 2
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        tmap = Counter(t)
+        res = ''
+        maxlen = float('inf')
+        for i in range(len(s)):
+            mp = tmap.copy()
+            for j in range(i, len(s)):
+                if s[j] in mp:
+                    mp[s[j]] -= 1
+                    if mp[s[j]] == 0:
+                        del mp[s[j]]
+                if not mp and (j - i + 1) < maxlen:
+                    res = s[i: j + 1]
+                    maxlen = j - i + 1
+                    
+        return res
+
+                    
+
+        
 
 #Accepted
 
