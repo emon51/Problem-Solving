9fe0275cf7df3da254a116b1c2994c87b58e83a9
Emon51 <94897729+emon51@users.noreply.github.com>
2024-05-19 12:33:03 +0600

    Update LC_435. Non-overlapping Intervals.py

--- Intervals/LC_435. Non-overlapping Intervals.py
+++ Intervals/LC_435. Non-overlapping Intervals.py
@@ -1,3 +1,33 @@
+
+'''
+Given an array of intervals intervals where intervals[i] = [starti, endi], return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
+
+ 
+
+Example 1:
+
+Input: intervals = [[1,2],[2,3],[3,4],[1,3]]
+Output: 1
+Explanation: [1,3] can be removed and the rest of the intervals are non-overlapping.
+Example 2:
+
+Input: intervals = [[1,2],[1,2],[1,2]]
+Output: 2
+Explanation: You need to remove two [1,2] to make the rest of the intervals non-overlapping.
+Example 3:
+
+Input: intervals = [[1,2],[2,3]]
+Output: 0
+Explanation: You don't need to remove any of the intervals since they're already non-overlapping.
+ 
+
+Constraints:
+
+1 <= intervals.length <= 105
+intervals[i].length == 2
+-5 * 104 <= starti < endi <= 5 * 104
+'''
+
 #Using stack.
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
@@ -15,3 +45,21 @@ class Solution:
                 stack.append([s, e])
                 
         return res
+
+
+#Space Optimiztion
+#We have to track just previous ending point.
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+
+        intervals.sort()
+        prev_end = intervals[0][1]
+        res = 0
+        for s, e in intervals[1: ]:
+            if s < prev_end:
+                res += 1
+                prev_end = min(prev_end, e)
+            else:
+                prev_end = e
+        return res
+        
