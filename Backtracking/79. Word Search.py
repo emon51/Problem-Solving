2f8a4e66d4f7e4144316b69f729ebf70859b79f9
Emon51 <94897729+emon51@users.noreply.github.com>
2024-05-23 12:18:31 +0600

    Update 79. Word Search.py

--- Backtracking/79. Word Search.py
+++ Backtracking/79. Word Search.py
@@ -1,25 +1,30 @@
-
 class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
+    def exist(self, grid: List[List[str]], word: str) -> bool:
 
-        def _valid(r, c):
-            return r >= 0 and r < m and c >= 0 and c < n
 
-        def dfs(r, c, i, vis):
+        def fun(r, c, i, vis):
             if i == len(word):
                 return True 
+            
+            vis.add((r, c))
+            res = False
             for x, y in dirs:
-                dr, dc = r + x, c + y
-                if _valid(dr, dc) and (dr, dc) not in vis and board[dr][dc] == word[i]:
-                    vis.add((dr, dc))
-                    if dfs(dr, dc, i + 1, vis):
-                        return True
-                    vis.remove((dr, dc))  # Backtrack
+                dr, dc = r + x, c + y 
+                if (0 <= dr < m and 0 <= dc < n) and (dr, dc) not in vis and grid[dr][dc] == word[i]:
+                    res = res or fun(dr, dc, i + 1, vis)
+            vis.remove((r, c))
+            return res
 
-        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        m, n = len(board), len(board[0])
+
+
+        dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        m, n = len(grid), len(grid[0])
         for r in range(m):
             for c in range(n):
-                if board[r][c] == word[0] and dfs(r, c, 1, {(r, c)}):
-                    return True
+                if grid[r][c] == word[0]:
+                    res = fun(r, c, 1, set())
+                    if res:
+                        return True
         return False
+
+        
