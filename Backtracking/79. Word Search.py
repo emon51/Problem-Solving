76290d95e9c5b71e202a3b1dda2a762615b622fc
Emon51 <94897729+emon51@users.noreply.github.com>
2024-05-23 12:42:16 +0600

    Update 79. Word Search.py

--- Backtracking/79. Word Search.py
+++ Backtracking/79. Word Search.py
@@ -27,4 +27,35 @@ class Solution:
                         return True
         return False
 
+
+#Space Optimization
+class Solution:
+    def exist(self, grid: List[List[str]], word: str) -> bool:
+
+        def fun(r, c, i, vis):
+            if i == len(word):
+                return True 
+            
+            tmp = grid[r][c]
+            grid[r][c] = None
+            res = False
+            for x, y in dirs:
+                dr, dc = r + x, c + y 
+                if (0 <= dr < m and 0 <= dc < n) and (dr, dc) not in vis and grid[dr][dc] == word[i]:
+                    res = res or fun(dr, dc, i + 1, vis)
+            grid[r][c] = tmp
+            return res
+
+        dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        m, n = len(grid), len(grid[0])
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == word[0]:
+                    res = fun(r, c, 1, set())
+                    if res:
+                        return True
+        return False
+
+        
+
         
