b3f980a9609250c6fb49f37e1c9e9b70ce2df042
Emon51 <94897729+emon51@users.noreply.github.com>
2024-05-24 11:14:36 +0600

    Create LC_784. Letter Case Permutation.py

---
+++ Backtracking/LC_784. Letter Case Permutation.py
@@ -0,0 +1,46 @@
+
+'''
+Given a string s, you can transform every letter individually to be lowercase or uppercase to create another string.
+
+Return a list of all possible strings we could create. Return the output in any order.
+
+ 
+
+Example 1:
+
+Input: s = "a1b2"
+Output: ["a1b2","a1B2","A1b2","A1B2"]
+Example 2:
+
+Input: s = "3z4"
+Output: ["3z4","3Z4"]
+ 
+
+Constraints:
+
+1 <= s.length <= 12
+s consists of lowercase English letters, uppercase English letters, and digits.
+
+'''
+
+
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+
+        res = []
+        n = len(s)
+        def fun(i, p = ''):
+            nonlocal res
+            if i == n:
+                res.append(p)
+                return 
+            if not s[i].isdigit():
+                if s[i].isupper():
+                    fun(i + 1, p + s[i].lower())
+                else:
+                    fun(i + 1, p + s[i].upper())
+            fun(i + 1, p + s[i])
+        
+        fun(0)
+        return res
+        
