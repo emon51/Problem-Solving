94525a5de772a3628c56224b4d05a9cb7d7eca11
Emon51 <94897729+emon51@users.noreply.github.com>
2024-05-27 20:30:32 +0600

    Update GFG_boundaryTraversalOfBT.py

--- Binary_Tree/GFG_boundaryTraversalOfBT.py
+++ Binary_Tree/GFG_boundaryTraversalOfBT.py
@@ -1,4 +1,4 @@
-#User function Template for python3
+
 
 '''
 class Node:
@@ -10,37 +10,31 @@ class Node:
 class Solution:
     def printBoundaryView(self, root):
         
-        def _leaf(node):
-            return not node.left and not node.right
-        
         def left(node):
             cur = node 
-            while cur and not _leaf(cur):
+            while cur and (cur.left or cur.right):
                 left_nodes.append(cur.data)
-                if cur.left:
-                    cur = cur.left 
-                else:
-                    cur = cur.right 
+                cur = cur.left if cur.left else cur.right 
         
         def right(node):
             cur = node 
-            while cur and not _leaf(cur):
+            while cur and (cur.left or cur.right):
                 right_nodes.append(cur.data)
-                if cur.right:
-                    cur = cur.right 
-                else:
-                    cur = cur.left 
+                cur = cur.right if cur.right else cur.left 
         
         def leaf(node):
-            if _leaf(node):
-                leaf_nodes.append(node.data)
-                return
-            if node.left:
-                leaf(node.left)
-            if node.right:
-                leaf(node.right)
+            stack = []
+            cur = node 
+            while cur or stack:
+                while cur:
+                    stack.append(cur)
+                    cur = cur.left
+                node = stack.pop()
+                if not node.left and not node.right:
+                    leaf_nodes.append(node.data)
+                cur = node.right
         
-        if _leaf(root):
+        if not root.left and not root.right:
             return [root.data]
             
         left_nodes = []
@@ -51,4 +45,108 @@ class Solution:
         right(root.right)
         leaf(root)
         
-        return [root.data] + left_nodes + leaf_nodes + right_nodes[::-1]
+        return [root.data] + left_nodes + leaf_nodes + right_nodes[::-1]
+
+
+
+
+#Problem from coding ninjas: https://www.naukri.com/code360/problems/boundary-traversal-of-binary-tree_790725?leftPanelTabValue=PROBLEM
+
+# class BinaryTreeNode:
+#     def __init__(self, data):
+#         self.data = data
+#         self.left = None
+#         self.right = None
+
+
+# Functions to traverse on each part.
+def traverseBoundary(root):
+
+    if not root:
+        return []
+    if not root.left and not root.right:
+        return [root.data]
+    
+    lefts = []
+    bottom = []
+    rights = []
+
+    #Extract left side
+    curr = root.left
+    while curr and (curr.left or curr.right):
+        lefts.append(curr.data)
+        if curr.left:
+            curr = curr.left 
+        else:
+             curr = curr.right 
+    
+    #Extract right side
+    curr = root.right
+    while curr and (curr.left or curr.right):
+        rights.append(curr.data)
+        if curr.right:
+            curr = curr.right
+        else:
+            curr = curr.left 
+    
+    #Extract leaves using inorder traversal
+    stack = []
+    curr = root
+    while curr or stack:
+        while curr:
+            stack.append(curr)
+            curr = curr.left 
+        node = stack.pop()
+        if not node.left and not node.right:
+            bottom.append(node.data)
+        curr = node.right
+    
+    
+    return [root.data] + lefts + bottom + rights[:: - 1]
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
