82778d5701e38474832039b9480a5a2b4f1ab164
Emon51 <94897729+emon51@users.noreply.github.com>
2024-05-28 11:49:14 +0600

    Update Tricks.py

--- Binary_Tree/Tricks.py
+++ Binary_Tree/Tricks.py
@@ -1,2 +1,3 @@
 
-1 >> All View (Vertical view, top view, bottom view) follows line algorithm associated with queue and map data structures.
+1 >> Vertical view, top view, bottom view follows vertical line algorithm associated with queue and map data structures.
+2 >> Left view, right view follows horizontal line algorithm associated with queue data structure.
