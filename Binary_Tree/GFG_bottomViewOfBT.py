f14aa7438115399858a0533127f2d952be877881
Emon51 <94897729+emon51@users.noreply.github.com>
2024-05-28 11:21:04 +0600

    Update GFG_bottomViewOfBT.py

--- Binary_Tree/GFG_bottomViewOfBT.py
+++ Binary_Tree/GFG_bottomViewOfBT.py
@@ -1,35 +1,87 @@
+'''
+Given a binary tree, print the bottom view from left to right.
+A node is included in bottom view if it can be seen when we look at the tree from bottom.
+
+                      20
+                    /    \
+                  8       22
+                /   \        \
+              5      3       25
+                    /   \      
+                  10    14
+
+For the above tree, the bottom view is 5 10 3 14 25.
+If there are multiple bottom-most nodes for a horizontal distance from root, then print the later one in level traversal. For example, in the below diagram, 3 and 4 are both the bottommost nodes at horizontal distance 0, we need to print 4.
+
+                      20
+                    /    \
+                  8       22
+                /   \     /   \
+              5      3 4     25
+                     /    \      
+                 10       14
+
+For the above tree the output should be 5 10 4 14 25.
+
+Note: The Input/Output format and Example given are used for the system's internal purpose, and should be used by a user for Expected Output only. As it is a function problem, hence a user should not read any input from the stdin/console. The task is to complete the function specified, and not to write the full code.
+ 
+
+Example 1:
+
+Input:
+       1
+     /   \
+    3     2
+Output: 3 1 2
+Explanation:
+First case represents a tree with 3 nodes
+and 2 edges where root is 1, left child of
+1 is 3 and right child of 1 is 2.
+
+Thus nodes of the binary tree will be
+printed as such 3 1 2.
+Example 2:
+
+Input:
+         10
+       /    \
+      20    30
+     /  \
+    40   60
+Output: 40 20 60 30
+Your Task:
+This is a functional problem, you don't need to care about input, just complete the function bottomView() which takes the root node of the tree as input and returns an array containing the bottom view of the given tree.
+
+Expected Time Complexity: O(N*logN).
+Expected Auxiliary Space: O(N).
+
+Constraints:
+1 <= Number of nodes <= 105
+1 <= Data of a node <= 105
+'''
+
 #This problem is a modified version of vertical order traversal, in vertical order traversal the node's values which is the LAST value of each vertical in sorted order corresponding to x-values is the required output 
 
 #User function Template for python3
-
-from collections import deque 
+from collections import deque
 class Solution:
     def bottomView(self, root):
-        if not root:
-            return [] 
         
-        q = deque() 
-        d = {} 
+        q = deque()
         q.append((root, 0))
-        
+        mapp = {}
         while q:
-            p, x = q.popleft()
-            if x in d:
-                d[x].append(p.data)
-            else:
-                d[x] = [p.data]
-            
-            if p.left:
-                q.append((p.left, x -1))
-            if p.right:
-                q.append((p.right, x +1))
+            node, line = q.popleft()
+            mapp[line] = node.data #Overwrite
+            if node.left:
+                q.append((node.left, line - 1))
+            if node.right:
+                q.append((node.right, line + 1))
         
         res = []
-        for key in sorted(d):
-            res.append(d[key][-1])
-        
+        for key in sorted(mapp):
+            res.append(mapp[key])
         return res
-        
         
         
         
