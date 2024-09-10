146bac89d57850005790353f42f8b95c314979cd
Emon51 <94897729+emon51@users.noreply.github.com>
2024-09-10 12:45:23 +0600

    Update LC_94_inorderTraversal.py

--- Binary_Tree/LC_94_inorderTraversal.py
+++ Binary_Tree/LC_94_inorderTraversal.py
@@ -19,4 +19,30 @@ class Solution:
           
       dfs(root) 
       return res
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+
+        if not root:
+            return
+        
+        res = []
+        stack = []
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+            node = stack.pop()
+            res.append(node.val)
+            root = node.right
+        return res
+        
         
