e6348a6d54c783ba29372f17cfe6511ba82eac8a
Emon51 <94897729+emon51@users.noreply.github.com>
2024-10-11 00:19:54 +0600

    Update GFG_Max distance between same elements.py

--- Two Pointers/GFG_Max distance between same elements.py
+++ Two Pointers/GFG_Max distance between same elements.py
@@ -19,7 +19,22 @@ Constraints:
 1 <= arr.size() <= 106
 1 <= arr[i] <= 109
 '''
+#Brute Force:
+class Solution:
+    # Your task is to Complete this function
+    # functtion should return an integer
+    def maxDistance(self, arr):
+        
+        n = len(arr)
+        res = 0
+        for i in range(n):
+            for j in range(n - 1, -1, -1):
+                if arr[i] == arr[j]:
+                    res = max(res, j - i)
+                    break 
+        return res 
 
+#Optimal
 class Solution:
     # Your task is to Complete this function
     # functtion should return an integer
@@ -35,4 +50,65 @@ class Solution:
         for i in range(n):
             res = max(res, mapp[arr[i]] - i)
         return res 
+
+
+#LC_962. Maximum Width Ramp
+'''
+A ramp in an integer array nums is a pair (i, j) for which i < j and nums[i] <= nums[j]. The width of such a ramp is j - i.
+
+Given an integer array nums, return the maximum width of a ramp in nums. If there is no ramp in nums, return 0.
+
+ 
+
+Example 1:
+
+Input: nums = [6,0,8,2,1,5]
+Output: 4
+Explanation: The maximum width ramp is achieved at (i, j) = (1, 5): nums[1] = 0 and nums[5] = 5.
+Example 2:
+
+Input: nums = [9,8,1,0,1,9,4,0,4,1]
+Output: 7
+Explanation: The maximum width ramp is achieved at (i, j) = (2, 9): nums[2] = 1 and nums[9] = 1.
+ 
+
+Constraints:
+
+2 <= nums.length <= 5 * 104
+0 <= nums[i] <= 5 * 104
+'''
+#Brute Force:
+class Solution:
+    def maxWidthRamp(self, nums: List[int]) -> int:
+
+        res = 0
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] <= nums[j]:
+                    res = max(res, j - i)
+        return res 
+        
+#Optimal:
+class Solution:
+    def maxWidthRamp(self, nums: List[int]) -> int:
+
+        n = len(nums)
+        next_max = [-1] * n 
+        next_max[n - 1] = nums[-1]
+        for i in range(n - 2, -1, -1):
+            next_max[i] = max(nums[i], next_max[i + 1])
+
+        res = 0 
+        i, j = 0, 0 
+        while i < n and j < n:
+            if nums[i] <= next_max[j]:
+                res = max(res, j - i)
+                j += 1
+            else:
+                i += 1
+        return res 
+
+        
+        
 
