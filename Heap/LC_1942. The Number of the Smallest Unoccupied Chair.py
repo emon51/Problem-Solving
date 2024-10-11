2a549264a4658de7b409629bc1a9c7b59ab05ef3
Emon51 <94897729+emon51@users.noreply.github.com>
2024-10-11 23:33:20 +0600

    Update LC_1942. The Number of the Smallest Unoccupied Chair.py

--- Heap/LC_1942. The Number of the Smallest Unoccupied Chair.py
+++ Heap/LC_1942. The Number of the Smallest Unoccupied Chair.py
@@ -45,3 +45,35 @@ times[i].length == 2
 0 <= targetFriend <= n - 1
 Each arrivali time is distinct.
 '''
+
+import heapq 
+
+class Solution:
+    def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
+        friends = [[friend_no, data[0], data[1]] for friend_no, data in enumerate(times)]
+        friends.sort(key=lambda c: c[1])
+        
+        # Initialize a heap to manage the occupied chairs with (leaving time, chair number)
+        heap = []
+        # A min heap to manage available chairs
+        available_chairs = []
+        
+        ch = 0  # This tracks the next available chair number if no free chairs exist
+        
+        for friend_no, s, e in friends:
+            # Free up the chairs that have become available (leaving time <= arrival time)
+            while heap and heap[0][0] <= s:
+                prev_end, prev_ch = heapq.heappop(heap)
+                heapq.heappush(available_chairs, prev_ch)
+            
+            # Allocate the smallest available chair or the next new one
+            if available_chairs:
+                expected_ch = heapq.heappop(available_chairs)
+            else:
+                expected_ch = ch
+                ch += 1
+            
+            if friend_no == targetFriend:
+                return expected_ch
+            heapq.heappush(heap, [e, expected_ch])
+
