7998003483ece2c477c64bf33325ce9025e06e3f
Emon51 <94897729+emon51@users.noreply.github.com>
2024-10-21 23:16:23 +0600

    Update (Substring)LC: 131_Palindrome Partitioning.py

--- Backtracking/(Substring)LC: 131_Palindrome Partitioning.py
+++ Backtracking/(Substring)LC: 131_Palindrome Partitioning.py
@@ -50,4 +50,35 @@ class Solution:
         fun(0)
         return res
 
+
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+
+        def palindrome(ss):
+            l, r = 0, len(ss) - 1
+            while l < r:
+                if ss[l] != ss[r]:
+                    return False 
+                l += 1
+                r -= 1
+            return True 
+
+        res = []
+        def fun(s, i, p = []):
+            nonlocal res 
+            if i == len(s):
+                res.append(p[:])
+                return 
+            for j in range(i, len(s)):
+                substr = s[i: j + 1]
+                if palindrome(substr):
+                    p.append(substr)
+                    fun(s, j + 1, p)
+                    p.pop()
+        
+        fun(s, 0)
+        return res 
+        
+
         
