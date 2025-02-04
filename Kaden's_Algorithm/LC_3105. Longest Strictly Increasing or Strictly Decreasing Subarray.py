cadca3a058eddbe247b323ba716b777f09af74f6
Emon51 <94897729+emon51@users.noreply.github.com>
2025-02-04 09:38:36 +0600

    Create LC_3105. Longest Strictly Increasing or Strictly Decreasing Subarray.py

---
+++ Kaden's_Algorithm/LC_3105. Longest Strictly Increasing or Strictly Decreasing Subarray.py
@@ -0,0 +1,103 @@
+'''
+You are given an array of integers nums. Return the length of the longest 
+subarray
+ of nums which is either 
+strictly increasing
+ or 
+strictly decreasing
+.
+
+ 
+
+Example 1:
+
+Input: nums = [1,4,3,3,2]
+
+Output: 2
+
+Explanation:
+
+The strictly increasing subarrays of nums are [1], [2], [3], [3], [4], and [1,4].
+
+The strictly decreasing subarrays of nums are [1], [2], [3], [3], [4], [3,2], and [4,3].
+
+Hence, we return 2.
+
+Example 2:
+
+Input: nums = [3,3,3,3]
+
+Output: 1
+
+Explanation:
+
+The strictly increasing subarrays of nums are [3], [3], [3], and [3].
+
+The strictly decreasing subarrays of nums are [3], [3], [3], and [3].
+
+Hence, we return 1.
+
+Example 3:
+
+Input: nums = [3,2,1]
+
+Output: 3
+
+Explanation:
+
+The strictly increasing subarrays of nums are [3], [2], and [1].
+
+The strictly decreasing subarrays of nums are [3], [2], [1], [3,2], [2,1], and [3,2,1].
+
+Hence, we return 3.
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 50
+1 <= nums[i] <= 50
+'''
+
+#Brute Force 
+#TC: O(N*N); SC: O(1)
+class Solution:
+    def longestMonotonicSubarray(self, nums: List[int]) -> int:
+
+        n = len(nums)
+        res = 1
+        for i in range(n):
+            j = i + 1
+            while j < n and nums[j - 1] < nums[j]:
+                res = max(res, j - i + 1)
+                j += 1
+            j = i + 1
+            while j < n and nums[j] < nums[j - 1]:
+                res = max(res, j - i + 1)
+                j += 1
+        return res 
+
+
+#Kaden's Algorithm
+#TC: O(N); SC: O(1)
+class Solution:
+    def longestMonotonicSubarray(self, nums: List[int]) -> int:
+
+        res = 1
+        inc, dec = 1, 1
+        for i in range(1, len(nums)):
+            if nums[i - 1] < nums[i]:
+                inc += 1 
+                dec = 1
+            elif nums[i] < nums[i - 1]:
+                dec += 1
+                inc = 1
+            else:
+                inc, dec = 1, 1
+            res = max(res, inc, dec)
+        return res 
+
+            
+        
+
+        
